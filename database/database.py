d16902d4837476336dc7058fb8c3f62c6c99d581
Madara-Uchiha-001 <121989958+Madara-Uchiha-001@users.noreply.github.com>
2024-08-08 06:01:24 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,5 +1,5 @@
 import motor.motor_asyncio
-from config import DB_URI, DB_NAME
+from config import DB_URL, DB_NAME
 
 dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
 database = dbclient[DB_NAME]
