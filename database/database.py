430c5c9a0e35cc1890d207cb2bbcebdb9d5959e1
Madara-Uchiha-001 <121989958+Madara-Uchiha-001@users.noreply.github.com>
2024-08-20 17:53:33 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,6 +1,5 @@
 import motor.motor_asyncio
 from config import DB_URL, DB_NAME
-from datetime import datetime, timedelta
 
 dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URL)
 database = dbclient[DB_NAME]
@@ -9,7 +8,6 @@ database = dbclient[DB_NAME]
 user_data = database['users']
 
 # Collection for storing premium user data
-premium_data = database['premium_users']
 
 # Default values for user verification status
 default_verify = {
@@ -19,12 +17,6 @@ default_verify = {
     'link': ""
 }
 
-# Default values for premium user data
-default_premium = {
-    'user_id': 0,
-    'expiry_date': 0,
-    'subscription_duration': 0  # Duration in days
-}
 
 def new_user(id):
     return {
@@ -63,28 +55,3 @@ async def full_userbase():
 async def del_user(user_id: int):
     await user_data.delete_one({'_id': user_id})
     return
-
-# Functions for managing premium users
-
-def new_premium_user(user_id: int, duration: int):
-    return {
-        'user_id': user_id,
-        'expiry_date': (datetime.now() + timedelta(days=duration)).timestamp(),
-        'subscription_duration': duration
-    }
-
-async def add_premium_user(user_id: int, duration: int):
-    premium_user = new_premium_user(user_id, duration)
-    await premium_data.update_one({'user_id': user_id}, {'$set': premium_user}, upsert=True)
-
-async def remove_premium_user(user_id: int):
-    await premium_data.delete_one({'user_id': user_id})
-
-async def get_premium_users():
-    users = await premium_data.find().to_list(length=1000)  # Adjust the length as needed
-    return users
-
-async def is_premium(user_id: int):
-    user = await premium_data.find_one({'user_id': user_id})
-    return bool(user) and user['expiry_date'] > datetime.now().timestamp()
-
